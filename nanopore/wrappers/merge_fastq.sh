3e1f6b181de0b06411752f36bcd4d5324d5ff68b
Dan Fornika <dfornika@gmail.com>
2018-07-18 06:00:45 -0700

    Update merge_fastq scripts

--- nanopore/wrappers/merge_fastq.sh
+++ nanopore/wrappers/merge_fastq.sh
@@ -39,15 +39,17 @@ do
   esac
 done
 
+mkdir -p "${INPUT}"/merged
+
 if [ "$BARCODING" = false ]; then
-  qsub MERGE_FASTQ_QSUB_SCRIPT "${INPUT}" --pass
-  qsub MERGE_FASTQ_QSUB_SCRIPT "${INPUT}" --fail
+  qsub "${MERGE_FASTQ_QSUB_SCRIPT}" "${INPUT}" --pass
+  qsub "${MERGE_FASTQ_QSUB_SCRIPT}" "${INPUT}" --fail
 elif [ "$BARCODING" = true ]; then
   for BARCODE_ID in $( ls -1 "${INPUT}"/fastq/0/workspace/pass ); do
-    qsub MERGE_FASTQ_QSUB_SCRIPT "${INPUT}" --barcode_id "${BARCODE_ID}" --pass
+    qsub "${MERGE_FASTQ_QSUB_SCRIPT}" -i "${INPUT}" --barcode_id "$BARCODE_ID" --pass
   done
-  for BARCODE_ID in $( ls -1 "${INPUT}"/0/workspace/fail ); do
-    qsub MERGE_FASTQ_QSUB_SCRIPT "${INPUT}" --barcode_id "${BARCODE_ID}" --fail
+  for BARCODE_ID in $( ls -1 "${INPUT}"/fastq/0/workspace/fail ); do
+    qsub "{$MERGE_FASTQ_QSUB_SCRIPT}" -i "${INPUT}" --barcode_id "$BARCODE_ID" --fail
   done
 fi
 
