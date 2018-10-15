c754f0c7efe8f20e04256eec9c52067e99e810e2
Dan Fornika <dfornika@gmail.com>
2018-10-15 16:46:49 -0700

    Clean up albacore wrapper and add global config file

--- wrappers/albacore.sh
+++ wrappers/albacore.sh
@@ -2,30 +2,34 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-declare -i NUM_FAST5_DIRS
-declare -i LOWER_FAST5_DIR_NUM
-declare -i UPPER_FAST5_DIR_NUM
+source ../config.conf
+
+albacore_version="2.3.1"
+
+declare -i num_fast5_dirs
+declare -i lower_fast5_dir_num
+declare -i upper_fast5_dir_num
 
 # Constants
-declare -r ALBACORE_QSUB_SCRIPT="/home/dfornika/code/qsub-scripts/nanopore/albacore.qsub" # replace when we decide on a system-wid install location
-declare -r OUTPUT_BASE_DIR="/data/minion/basecalls"
-declare -r ALBACORE_SCRIPT="/opt/miniconda2/envs/nanopore/bin/read_fast5_basecaller.py"
-ALBACORE_VERSION=$( "${ALBACORE_SCRIPT}" --version | cut -d "(" -f2 | cut -d ")" -f1 | cut -d' ' -f2)
-declare -r QSUB_ERROR_LOG_DIR="/data/minion/basecalls/qsub_logs/$( date --iso-8601 )/albacore"
-declare -r QSUB_OUTPUT_LOG_DIR="/data/minion/basecalls/qsub_logs/$( date --iso-8601 )/albacore"
+declare -r albacore_qsub_script="${routine_nanopore_analysis_repo_root_dir}"/albacore.qsub
+declare -r output_base_dir="${minion_data_base_dir}"/basecalls
+declare -r albacore_script="${conda_envs_dir}"/albacore-"${albacore_version}"/bin/read_fast5_basecaller.py
+reported_albacore_version=$( "${albacore_script}" --version | cut -d "(" -f2 | cut -d ")" -f1 | cut -d' ' -f2)
+declare -r qsub_error_log_dir="${minion_data_base_dir}/basecalls/qsub_logs/$( date --iso-8601 )/albacore"
+declare -r qsub_output_log_dir="${minion_data_base_dir}/data/minion/basecalls/qsub_logs/$( date --iso-8601 )/albacore"
 
 # Defaults
-CONFIG="r94_450bps_linear.cfg"
-BARCODING=false
-LOWER_FAST5_DIR_NUM=0
-UPPER_FAST5_DIR_NUM=-1
+albacore_config="r94_450bps_linear.cfg"
+barcoding=false
+lower_fast5_dir_num=0
+upper_fast5_dir_num=-1
 
-USAGE="$( basename $BASH_SOURCE ) [-h] [-c config] [-b|--barcoding] [-l|--lower_fast5_dir_num] [-u|--upper_fast5_dir_num] -i|--input <inputdir>"
+usage="$( basename $BASH_SOURCE ) [-h] [-c config] [-b|--barcoding] [-l|--lower_fast5_dir_num] [-u|--upper_fast5_dir_num] -i|--input <inputdir>"
 
 if [[ $# -eq 0 || $1 == "--help" ||  $1 == "-h" ]] 
 then 
-    echo "Usage: ${USAGE}"
-    echo "NOTE: Default config is: ${CONFIG}"
+    echo "Usage: ${usage}"
+    echo "NOTE: Default config is: ${albacore_config}"
     exit 0
 fi
 
@@ -38,27 +42,27 @@ do
   case $key in
     -i|--input)
     # Base directory for run (must contain a 'fast5' subdir)
-    INPUT="$2"
+    input_dir="$2"
     shift # past argument
     shift # past value
     ;;
     -c|--config)
-    CONFIG="$2"
+    albacore_config="$2"
     shift # past argument
     shift # past value
     ;;
     -b|--barcoding)
     # No value, just the argument
-    BARCODING=true
+    barcoding=true
     shift # past argument
     ;;
     -l|--lower_fast5_dir_num)
-    LOWER_FAST5_DIR_NUM="$2"
+    lower_fast5_dir_num="$2"
     shift # past argument
     shift # past value
     ;;
     -u|--upper_fast5_dir_num)
-    UPPER_FAST5_DIR_NUM="$2"
+    upper_fast5_dir_num="$2"
     shift # past argument
     shift # past value
     ;;
@@ -69,9 +73,9 @@ do
   esac
 done
 
-NUM_FAST5_SUBDIRS=$( ls -1 "$INPUT"/fast5 | wc -l )
+num_fast5_subdirs=$( ls -1 "$input_dir"/fast5 | wc -l )
 
-if [[ "${UPPER_FAST5_DIR_NUM:-}" > -1 && "${LOWER_FAST5_DIR_NUM}" > "${UPPER_FAST5_DIR_NUM}" ]];
+if [[ "${lower_fast5_dir_num:-}" > -1 && "${lower_fast5_dir_num}" > "${upper_fast5_dir_num}" ]];
 then
     echo "ERROR: Upper fast5 dir must be greater than or equal to Lower fast5 dir."
     exit 1
@@ -80,28 +84,34 @@ fi
 # Prepare parent directory with no timestamp (date only)
 # Multiple run re-starts will be collected inside the same parent dir
 # YYYYMMDD_sample_libraryprep/YYYYMMDD_TIME_sample_libraryprep
-INPUT_BASENAME=$( basename "${INPUT}" )
-INPUT_PARENT_BASENAME=$( basename $( dirname "${INPUT}" ) )
+input_dir_basename=$( basename "${input_dir}" )
+input_dir_parent_basename=$( basename $( dirname "${input_dir}" ) )
 
-mkdir -p "${OUTPUT_BASE_DIR}"/"${INPUT_PARENT_BASENAME}"
+mkdir -p "${output_base_dir}"/"${input_dir_parent_basename}"
 
 # Prepare log dirs
-mkdir -p "${QSUB_ERROR_LOG_DIR}"
-mkdir -p "${QSUB_OUTPUT_LOG_DIR}"
+mkdir -p "${qsub_error_log_dir}"
+mkdir -p "${qsub_output_log_dir}"
 
 # Print some info to stderr for debugging & provenance
-(>&2 echo INPUT               = "${INPUT}" )
-(>&2 echo INPUT_BASENAME      = "${INPUT_BASENAME}" )
-(>&2 echo INPUT_PARENT_BASENAME = "${INPUT_PARENT_BASENAME}" )
-(>&2 echo NUM_FAST5_SUBDIRS   = "${NUM_FAST5_SUBDIRS}" )
-(>&2 echo LOWER_FAST5_DIR_NUM = "${LOWER_FAST5_DIR_NUM}" "(Default is 0 if unspecified)")
-(>&2 echo UPPER_FAST5_DIR_NUM = "${UPPER_FAST5_DIR_NUM}" "(Default is -1 if unspecified and upper limit will be auto-detected)")
-(>&2 echo CONFIG              = "${CONFIG}" )
-(>&2 echo BARCODING           = "${BARCODING}" )
-(>&2 echo ALBACORE_VERSION    = "${ALBACORE_VERSION}" )
-(>&2 echo QSUB_ERROR_LOG_DIR  = "${QSUB_ERROR_LOG_DIR}" )
-(>&2 echo QSUB_OUTPUT_LOG_DIR = "${QSUB_OUTPUT_LOG_DIR}" )
+(>&2 echo "Input directory:" "${input_dir}" )
+(>&2 echo "Basename of input directory:" "${input_dir_basename}" )
+(>&2 echo "Basename of input directory parent directory:" "${input_dir_parent_basename}" )
+(>&2 echo "Number of fast5 sub-directories to basecall:" "${num_fast5_subdirs}" )
+(>&2 echo "Lower-bound fast5 directory number:" "${lower_fast5_dir_num}" "(Default is 0 if unspecified)")
+(>&2 echo "Upper-bound fast5 directory number:" "${upper_fast5_dir_num}" "(Default is -1 if unspecified and upper limit will be auto-detected)")
+(>&2 echo "Albacore config:" "${albacore_config}" )
+(>&2 echo "Demultiplex barcodes:" "${barcoding}" )
+(>&2 echo "Albacore version:"    = "${reported_albacore_version}" )
+(>&2 echo "qsub Error Log directory:" "${qsub_error_log_dir}" )
+(>&2 echo "qsub Standard Output Log directory:" "${qsub_output_log_dir}" )
 
 # Submit qsub job
-qsub -o "${QSUB_OUTPUT_LOG_DIR}" -e "${QSUB_ERROR_LOG_DIR}" -t $(( $LOWER_FAST5_DIR_NUM + 1 )):$( if [[ "${UPPER_FAST5_DIR_NUM}" = -1 ]]; then echo $NUM_FAST5_SUBDIRS; else echo $(( $UPPER_FAST5_DIR_NUM + 1 )); fi ) "${ALBACORE_QSUB_SCRIPT}" -c "${CONFIG}" -i "${INPUT}" -o "${OUTPUT_BASE_DIR}"/"${INPUT_PARENT_BASENAME}"/"${INPUT_BASENAME}"/albacore-"${ALBACORE_VERSION}"_$( cut -d '.' -f1 <<< "${CONFIG}" ) $( if [ "$BARCODING" = true ]; then echo "--barcoding"; fi )
+qsub -o "${qsub_output_log_dir}" -e "${qsub_error_log_dir}" \
+     -t $(( $lower_fast5_dir_num + 1 )):$( if [[ "${upper_fast5_dir_num}" = -1 ]]; then echo $num_fast5_subdirs; else echo $(( $upper_fast5_dir_num + 1 )); fi ) \
+     "${albacore_qsub_script}" \
+     -c "${albacore_config}" \
+     -i "${input_dir}" \
+     -o "${output_base_dir}"/"${input_dir_parent_basename}"/"${input_dir_basename}"/albacore-"${albacore_version}"_$( cut -d '.' -f1 <<< "${albacore_config}" ) \
+     $( if [ "$barcoding" = true ]; then echo "--barcoding"; fi )
 
